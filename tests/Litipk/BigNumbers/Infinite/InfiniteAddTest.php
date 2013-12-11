1fd7d7539e991c1c9d8ba10e09659eb72dd62c1e
Andreu Correa Casablanca <castarco@gmail.com>
2013-12-12 00:19:28 +0100

    Added two tests

--- tests/Litipk/BigNumbers/Infinite/InfiniteAddTest.php
+++ tests/Litipk/BigNumbers/Infinite/InfiniteAddTest.php
@@ -30,4 +30,25 @@ class InfiniteAddTest extends PHPUnit_Framework_TestCase
 		$this->assertTrue($pInf->add($nInf)->isNaN());
 		$this->assertTrue($nInf->add($pInf)->isNaN());
 	}
+
+	public function testDecimalSub ()
+	{
+		$pInf = Infinite::getPositiveInfinite();
+		$nInf = Infinite::getNegativeInfinite();
+
+		$pTen = Decimal::fromInteger(10);
+		$nTen = Decimal::fromInteger(-10);
+
+		$this->assertTrue($pInf->add($pTen)->equals($pInf));
+		$this->assertTrue($nInf->add($pTen)->equals($nInf));
+
+		$this->assertTrue($pInf->add($nTen)->equals($pInf));
+		$this->assertTrue($nInf->add($nTen)->equals($nInf));
+
+		$this->assertTrue($pTen->add($pInf)->equals($pInf));
+		$this->assertTrue($pTen->add($nInf)->equals($nInf));
+
+		$this->assertTrue($nTen->add($pInf)->equals($pInf));
+		$this->assertTrue($nTen->add($nInf)->equals($nInf));
+	}
 }
--- tests/Litipk/BigNumbers/Infinite/InfiniteSubTest.php
+++ tests/Litipk/BigNumbers/Infinite/InfiniteSubTest.php
@@ -30,4 +30,25 @@ class InfiniteSubTest extends PHPUnit_Framework_TestCase
 		$this->assertTrue($nan->sub($pInf)->isNaN());
 		$this->assertTrue($nan->sub($nInf)->isNaN());
 	}
+
+	public function testDecimalSub ()
+	{
+		$pInf = Infinite::getPositiveInfinite();
+		$nInf = Infinite::getNegativeInfinite();
+
+		$pTen = Decimal::fromInteger(10);
+		$nTen = Decimal::fromInteger(-10);
+
+		$this->assertTrue($pInf->sub($pTen)->equals($pInf));
+		$this->assertTrue($nInf->sub($pTen)->equals($nInf));
+
+		$this->assertTrue($pInf->sub($nTen)->equals($pInf));
+		$this->assertTrue($nInf->sub($nTen)->equals($nInf));
+
+		$this->assertTrue($pTen->sub($pInf)->equals($nInf));
+		$this->assertTrue($pTen->sub($nInf)->equals($pInf));
+
+		$this->assertTrue($nTen->sub($pInf)->equals($nInf));
+		$this->assertTrue($nTen->sub($nInf)->equals($pInf));
+	}
 }
