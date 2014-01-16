72ee4e59500a59d37bfb13fcf02a553473603f14
Andreu Correa Casablanca <castarco@gmail.com>
2014-01-16 22:53:29 +0100

    Updated php_codesniffer dependency

---
+++ tests/Litipk/BigNumbers/Decimal/DecimalSerializationTest.php
@@ -0,0 +1,21 @@
+<?php
+
+use Litipk\BigNumbers\Decimal as Decimal;
+
+class DecimalSerializationTest extends PHPUnit_Framework_TestCase
+{
+	public function testBaseCase ()
+	{
+		$one = Decimal::fromInteger(1);
+		$little = Decimal::fromString('0.0000000000001');
+
+		$serialized_one = serialize($one);
+		$unserialized_one = unserialize($serialized_one);
+
+		$serialized_little = serialize($little);
+		$unserialized_little = unserialize($serialized_little);
+
+		$this->assertTrue($one->equals($unserialized_one));
+		$this->assertTrue($little->equals($unserialized_little));
+	}
+}
