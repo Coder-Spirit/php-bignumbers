38ed44586b3a01887518c7090be8a94f5224c903
Andreu Correa Casablanca <castarco@gmail.com>
2014-01-26 17:35:15 +0100

    Minor style fixes

--- tests/Decimal/DecimalAbsTest.php
+++ tests/Decimal/DecimalAbsTest.php
@@ -17,12 +17,12 @@ class DecimalAbsTest extends PHPUnit_Framework_TestCase
 
     public function testInfiniteAbs()
     {
-    	$this->assertTrue(
-    		Decimal::getPositiveInfinite()->abs()->equals(Decimal::getPositiveInfinite())
-    	);
+        $this->assertTrue(
+            Decimal::getPositiveInfinite()->abs()->equals(Decimal::getPositiveInfinite())
+        );
 
-    	$this->assertTrue(
-    		Decimal::getNegativeInfinite()->abs()->equals(Decimal::getPositiveInfinite())
-    	);
+        $this->assertTrue(
+            Decimal::getNegativeInfinite()->abs()->equals(Decimal::getPositiveInfinite())
+        );
     }
 }
