050d4c5ece5a89c101ed18804b41c478e8503f94
Andreu Correa Casablanca <castarco@gmail.com>
2015-03-25 20:12:20 +0100

    Added a new test for InfiniteDecimal casts

--- tests/Decimal/DecimalAsNativeTest.php
+++ tests/Decimal/DecimalAsNativeTest.php
@@ -8,6 +8,21 @@ date_default_timezone_set('UTC');
 
 class DecimalAsFloatTest extends PHPUnit_Framework_TestCase
 {
+    public function testAsInteger()
+    {
+        $this->assertEquals(1, Decimal::fromString('1.0')->asInteger());
+        $this->assertTrue(is_int(Decimal::fromString('1.0')->asInteger()));
+
+        $this->assertEquals(1, Decimal::fromInteger(1)->asInteger());
+        $this->assertTrue(is_int(Decimal::fromInteger(1)->asInteger()));
+
+        $this->assertEquals(1, Decimal::fromFloat(1.0)->asInteger());
+        $this->assertEquals(1, Decimal::fromString('1.123123123')->asInteger());
+
+        $this->assertTrue(is_int(Decimal::fromFloat(1.0)->asInteger()));
+        $this->assertTrue(is_int(Decimal::fromString('1.123123123')->asInteger()));
+    }
+
     public function testAsFloat()
     {
         $this->assertEquals(1.0, Decimal::fromString('1.0')->asFloat());
