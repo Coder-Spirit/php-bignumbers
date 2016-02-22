56aeab3b0bed806ad916f9ea0ae324086d859113
codisart <louis.celier@gmail.com>
2016-02-22 17:40:44 +0100

    [TECH] Add two new methods and their tests
    - arcsec : computes the arcsecant
    - arccsc : computes the arccosecant

---
+++ tests/Decimal/DecimalArccscTest.php
@@ -0,0 +1,42 @@
+<?php
+
+use Litipk\BigNumbers\Decimal as Decimal;
+
+/**
+ * @group arccsc
+ */
+class DecimalArccscTest extends PHPUnit_Framework_TestCase
+{
+    public function arccscProvider() {
+        // Some values provided by wolframalpha
+        return [
+            ['25.546', '0.03915507577327', 14],
+            ['1.5', '0.729728', 6],
+            ['1', '1.57079632679489662', 17],
+            ['-1', '-1.57079632679489662', 17],
+        ];
+    }
+
+    /**
+     * @dataProvider arccscProvider
+     */
+    public function testSimple($nr, $answer, $digits)
+    {
+        $x = Decimal::fromString($nr);
+        $arccscX = $x->arccsc($digits);
+
+        $this->assertTrue(
+            Decimal::fromString($answer)->equals($arccscX),
+            "The answer must be " . $answer . ", but was " . $arccscX
+        );
+    }
+
+    /**
+     * @expectedException \DomainException
+     * @expectedExceptionMessage The arccosecant of this number is undefined.
+     */
+    public function testArccscBetweenOneAndNegativeOne()
+    {
+        Decimal::fromString('0.546')->arccsc();
+    }
+}
