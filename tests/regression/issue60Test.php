590ef624f36482b2fb8cca8a9f6ae2374cd36d5c
Adam Benson <adam.benson@bigcommerce.com>
2017-08-21 17:45:13 +1000

    ISSUE-60: Fix for division by zero caused by float precision
    Aims to resolve https://github.com/Litipk/php-bignumbers/issues/60

---
+++ tests/regression/issue60Test.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+use Litipk\BigNumbers\Decimal;
+use PHPUnit\Framework\TestCase;
+
+class issue60Test extends TestCase
+{
+    public function test_that_fromFloat_division_does_not_calculate_invalid_log10_avoiding_div_zero()
+    {
+        $value = Decimal::fromFloat(1.001);
+        $divisor = Decimal::fromFloat(20);
+
+        $this->assertEquals(0.05005, $value->div($divisor)->asFloat());
+        $this->assertEquals(0.000434077479319, $value->log10()->asFloat());
+    }
+}
